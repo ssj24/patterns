5d01ae24b04de3563f4ce931e10b402bd7d60a4a
ssj24 <sujee0824@gmail.com>
2024-08-21 17:03:10 +0900

    Feat: make star moves

--- lib/widgets/solar_system.dart
+++ lib/widgets/solar_system.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,40 +10,159 @@ class SolarSystem extends StatefulWidget {
   State<SolarSystem> createState() => _SolarSystemState();
 }
 
-class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _SolarSystemState extends State<SolarSystem> with TickerProviderStateMixin {
+  late AnimationController _starController;
+  late AnimationController _starShimeringController;
+  late Animation<double> _opacityAnimation1;
+  late Animation<double> _opacityAnimation2;
+  late Animation<double> _opacityAnimation3;
+  List<Widget> _smallWidgets = [];
+  List<Widget> _mediumWidgets = [];
+  List<Widget> _largeWidgets = [];
+  List<int> starPeriods = [2, 5, 13];
+  List<int> starCounts = [50, 20, 10];
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
+    _starController = AnimationController(
       duration: const Duration(seconds: 4),
       vsync: this,
     )..repeat();
+    _starShimeringController = AnimationController(
+      duration: const Duration(seconds: 4),
+      vsync: this,
+    )..repeat(reverse: true);
+    _opacityAnimation1 = Tween<double>(begin: 0.8, end: 0.3).animate(_starShimeringController);
+    _opacityAnimation2 = Tween<double>(begin: 0.3, end: 0.5).animate(_starShimeringController);
+    _opacityAnimation3 = Tween<double>(begin: 0.1, end: 0.7).animate(_starShimeringController);
+
+    _smallWidgets = _generatePositionedSmallWidgets(starCounts[0]);
+    _mediumWidgets = _generatePositionedMediumWidgets(starCounts[1]);
+    _largeWidgets = _generatePositionedLargeWidgets(starCounts[2]);
+    for (var i=0; i<starPeriods.length; i++) {
+      Timer.periodic(Duration(seconds: starPeriods[i]), (Timer timer) {
+        setState(() {
+          if (i == 0) {
+            _smallWidgets = _generatePositionedSmallWidgets(starCounts[i]);
+          } else if (i == 1) {
+            _mediumWidgets = _generatePositionedMediumWidgets(starCounts[i]);
+          } else {
+            _largeWidgets = _generatePositionedLargeWidgets(starCounts[i]);
+          }
+        });
+      });
+    }
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _starController.dispose();
+    _starShimeringController.dispose();
     super.dispose();
   }
 
-  List<Positioned> generatePositionedWidgets(int count) {
+  List<Positioned> _generatePositionedSmallWidgets(int count) {
     return List<Positioned>.generate(count, (index) {
       double top = Random().nextDouble() * 400; // Random top position between 0 and 200
       double left = Random().nextDouble() * 400;
-      double width = Random().nextDouble() * 10;
-      double height = Random().nextDouble() * 20;
+      double width = Random().nextDouble() * 7;
+      double customOpacity = Random().nextDouble();
       return Positioned(
         top: top,
         left: left,
-        child: Container(
-          width: width,
-          height: width,
-          decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(50)
-          ),
+        child: AnimatedBuilder(
+          animation: _starShimeringController,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityAnimation1.value,
+              child: Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity(),
+                  // ..scale(_scaleAnimation.value)
+                  // ..rotateZ(_rotationAnimation.value),
+                child: Container(
+                    width: width,
+                    height: width,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(customOpacity),
+                        borderRadius: BorderRadius.circular(50)
+                    )
+                ),
+              ),
+            );
+          },
         ),
+      );
+    });
+  }
+  List<Positioned> _generatePositionedMediumWidgets(int count) {
+    return List<Positioned>.generate(count, (index) {
+      double value = Random().nextDouble();
+      double top = Random().nextDouble() * 400; // Random top position between 0 and 200
+      double left = Random().nextDouble() * 400;
+      double width = value * 13;
+      double customOpacity = value > 0.5 ? value - 0.45 : value;
+      return Positioned(
+        top: top,
+        left: left,
+        child: AnimatedBuilder(
+          animation: _starShimeringController,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityAnimation2.value,
+              child: Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity(),
+                // ..scale(_scaleAnimation.value)
+                // ..rotateZ(_rotationAnimation.value),
+                child: Container(
+                  width: width,
+                  height: width,
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(customOpacity),
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                ),
+              ),
+            );
+          }
+        ),
+      );
+    });
+  }
+  List<Positioned> _generatePositionedLargeWidgets(int count) {
+    return List<Positioned>.generate(count, (index) {
+      double value = Random().nextDouble();
+      double top = Random().nextDouble() * 400; // Random top position between 0 and 200
+      double left = Random().nextDouble() * 400;
+      double width = value * 15;
+      double customOpacity = value > 0.5 ? value - 0.495 : value;
+      return Positioned(
+        top: top,
+        left: left,
+        child: AnimatedBuilder(
+          animation: _starShimeringController,
+          builder: (context, child)
+      {
+        return Opacity(
+          opacity: _opacityAnimation3.value,
+          child: Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity(),
+            // ..scale(_scaleAnimation.value)
+            // ..rotateZ(_rotationAnimation.value),
+            child: Container(
+              width: width,
+              height: width,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(customOpacity),
+                  borderRadius: BorderRadius.circular(50)
+              ),
+            ),
+          ),
+        );
+      }
+      )
       );
     });
   }
@@ -58,6 +178,9 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
         height: 400,
         child: Stack(
           children: [
+            ..._smallWidgets,
+            ..._mediumWidgets,
+            ..._largeWidgets,
             Positioned(
               left: -200,
               top: 50,
@@ -69,49 +192,41 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
                     borderColor: Colors.white,
                     borderWidth: 2,
                   ),
-                  child: SizedBox(
-                    width: 500,
-                    height: 300,
-                    child: AnimatedBuilder(
-                      animation: _controller,
-                      builder: (context, child) {
-                        final angle = _controller.value * 2 * pi;
-                        final x = 250 + 250 * cos(angle);
-                        final y = 150 + 150 * sin(angle);
-                        return Stack(
-                          children: [
-                            Positioned(
-                              left: x - 10,
-                              top: y - 10,
-                              child: child!,
-                            ),
-                          ],
-                        );
-                      },
-                      child: Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    ),
-                  ),
                 ),
               )
             ),
             Positioned(
-              left: 200,
-              top: 70,
-              child: Container(
-                width: 20,
-                height: 20,
-                decoration: BoxDecoration(
-                  color: Colors.yellow.shade900,
-                  borderRadius: BorderRadius.circular(50)
+              left: -200,
+              top: 50,
+              child: SizedBox(
+              width: 500,
+              height: 300,
+              child: AnimatedBuilder(
+                animation: _starController,
+                builder: (context, child) {
+                  final angle = _starController.value * 2 * pi;
+                  final x = 250 + 230 * cos(angle);
+                  final y = 150 + 140 * sin(angle);
+                  return Stack(
+                    children: [
+                      Positioned(
+                        left: x - 10,
+                        top: y - 10,
+                        child: child!,
+                      ),
+                    ],
+                  );
+                },
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
                 ),
               ),
+            )
             ),
             Positioned(
                 left: -150,
@@ -128,6 +243,18 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
                 )
             ),
             Positioned(
+              left: 200,
+              top: 118,
+              child: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Colors.yellow.shade900,
+                  borderRadius: BorderRadius.circular(50)
+                ),
+              ),
+            ),
+            Positioned(
                 left: -130,
                 top: 90,
                 child: Transform.rotate(
@@ -137,6 +264,39 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
                     painter: OvalBorderPainter(
                       borderColor: Colors.white,
                       borderWidth: 2,
+                    ),
+                  ),
+                )
+            ),
+            Positioned(
+                left: -200,
+                top: 50,
+                child: SizedBox(
+                  width: 500,
+                  height: 300,
+                  child: AnimatedBuilder(
+                    animation: _starController,
+                    builder: (context, child) {
+                      final angle = _starController.value * 2 * pi;
+                      final x = 250 + 180 * cos(angle);
+                      final y = 150 + 100 * sin(angle);
+                      return Stack(
+                        children: [
+                          Positioned(
+                            left: x - 10,
+                            top: y - 10,
+                            child: child!,
+                          ),
+                        ],
+                      );
+                    },
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        shape: BoxShape.circle,
+                      ),
                     ),
                   ),
                 )
@@ -156,6 +316,39 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
                 )
             ),
             Positioned(
+                left: -200,
+                top: 50,
+                child: SizedBox(
+                  width: 500,
+                  height: 300,
+                  child: AnimatedBuilder(
+                    animation: _starController,
+                    builder: (context, child) {
+                      final angle = _starController.value * 2 * pi;
+                      final x = 250 + 160 * cos(angle);
+                      final y = 150 + 90 * sin(angle);
+                      return Stack(
+                        children: [
+                          Positioned(
+                            left: x - 10,
+                            top: y - 10,
+                            child: child!,
+                          ),
+                        ],
+                      );
+                    },
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                )
+            ),
+            Positioned(
                 left: -100,
                 top: 130,
                 child: Transform.rotate(
@@ -165,6 +358,39 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
                     painter: OvalBorderPainter(
                       borderColor: Colors.white,
                       borderWidth: 2,
+                    ),
+                  ),
+                )
+            ),
+            Positioned(
+                left: -210,
+                top: 50,
+                child: SizedBox(
+                  width: 500,
+                  height: 300,
+                  child: AnimatedBuilder(
+                    animation: _starController,
+                    builder: (context, child) {
+                      final angle = _starController.value * 2 * pi;
+                      final x = 250 + 90 * cos(angle);
+                      final y = 150 + 60 * sin(angle);
+                      return Stack(
+                        children: [
+                          Positioned(
+                            left: x -10,
+                            top: y - 10,
+                            child: child!,
+                          ),
+                        ],
+                      );
+                    },
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
                     ),
                   ),
                 )
@@ -183,7 +409,40 @@ class _SolarSystemState extends State<SolarSystem>  with SingleTickerProviderSta
                   ),
                 )
             ),
-            ...generatePositionedWidgets(30)
+            Positioned(
+                left: -240,
+                top: 50,
+                child: SizedBox(
+                  width: 500,
+                  height: 300,
+                  child: AnimatedBuilder(
+                    animation: _starController,
+                    builder: (context, child) {
+                      final angle = _starController.value * 2 * pi;
+                      final x = 250 + 60 * cos(angle);
+                      final y = 150 + 30 * sin(angle);
+                      return Stack(
+                        children: [
+                          Positioned(
+                            left: x - 10,
+                            top: y - 10,
+                            child: child!,
+                          ),
+                        ],
+                      );
+                    },
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.purple,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                )
+            ),
+
 
           ],
         )
@@ -205,7 +464,7 @@ class OvalBorderPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = borderWidth;
 
-    final Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    final Rect rect = Rect.fromLTWH(0, 10, size.width-20, size.height-20);
     canvas.drawOval(rect, paint);
   }
 
